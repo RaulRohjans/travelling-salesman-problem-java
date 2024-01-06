6bce9a38fa1148e44f3a36133348b1d8ef9bf9c9
Raul Rohjans <rauljann@hotmail.com>
2024-01-06 17:56:47 +0000

    Initial convertion of the old algo in C to Java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,122 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Random;
+
+import static utils.Helper.*;
+
 
 public class Main {
+
+    private static final int POSSIBLE_TOURS_SIZE = 10;
+    private static final int MAX_GENERATIONS = 1000; // Number of times the possible tours will be improved
+    private static final double CROSSOVER_RATE = 0.7;
+    private static final double MUTATION_RATE = 0.2;
+    private static final double DE_FACTOR = 0.5;
+
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        // Start log
+        long begin = System.currentTimeMillis();
+
+        /* --- Init Program Data --- */
+        /*if(args.length < 5) {
+            System.out.println("Please make sure all arguments are present!\n");
+            System.out.println("java -jar tsp.jar <inputFile> <threadsNumber> <maxExecutionTime> " +
+                    "<populationSize> <mutationRate>");
+            return;
+        }
+
+        String inputFilePath = args[0];
+        int threadsNumber = Integer.parseInt(args[1]);
+        int maxExecTime = Integer.parseInt(args[2]);
+        int populationSize = Integer.parseInt(args[3]);
+        double MUTATION_RATE = Double.parseDouble(args[4]);
+
+        Map<Integer, int[][]> inputValues = ReadInputFileData(inputFilePath);
+        int cityMapSize = inputValues.entrySet().stream().findFirst().get().getKey();
+        int[][] cityMap = inputValues.entrySet().stream().findFirst().get().getValue();*/
+        int cityMapSize = 5;
+        int[][] cityMap = LoadTestCities();
+
+        int[][] possibleTours = new int[POSSIBLE_TOURS_SIZE][cityMapSize];
+        int[] distances = new int[POSSIBLE_TOURS_SIZE];
+        Arrays.fill(distances, Integer.MAX_VALUE);
+
+        Random rand = new Random();
+        /* ---------------------- */
+
+        /* Initialize possible tours with random values */
+        for (int i = 0; i < POSSIBLE_TOURS_SIZE; ++i) {
+            StartRandomTour(possibleTours[i], cityMapSize);
+            distances[i] = CalcTourDistance(possibleTours[i], cityMap, cityMapSize);
+        }
+        /* -------------------------------------------- */
+
+        /* --- Start Differential Evolution Algo --- */
+        for (int generation = 0; generation < MAX_GENERATIONS; ++generation) {
+            for (int i = 0; i < POSSIBLE_TOURS_SIZE; ++i) {
+                // Select three indexes for the crossover process
+                int targetIndex, alterIndex1, alterIndex2;
+                do {
+                    targetIndex = rand.nextInt(POSSIBLE_TOURS_SIZE);
+                    alterIndex1 = rand.nextInt(POSSIBLE_TOURS_SIZE);
+                    alterIndex2 = rand.nextInt(POSSIBLE_TOURS_SIZE);
+                } while (alterIndex1 == targetIndex || alterIndex2 == targetIndex || alterIndex1 == alterIndex2);
+
+                // Create an altered individual by combining the other two into this one
+                for (int j = 0; j < cityMapSize; ++j) {
+                    if (rand.nextDouble() < CROSSOVER_RATE || j == cityMapSize - 1) {
+                        possibleTours[i][j] = (int) (possibleTours[alterIndex1][j] + DE_FACTOR * (possibleTours[alterIndex2][j] - possibleTours[alterIndex1][j]));
+                    }
+
+                    // Repair process
+                    for (int k = 0; k < j; ++k) {
+                        if (possibleTours[i][j] == possibleTours[i][k]) {
+                            possibleTours[i][j] = GenerateNewUniqueCity(possibleTours[i], cityMapSize, j);
+                            k = -1;
+                        }
+                    }
+                }
+
+                // Enforce value check
+                for (int j = 0; j < cityMapSize; ++j) {
+                    if (possibleTours[i][j] < 0) {
+                        possibleTours[i][j] = 0;
+                    } else if (possibleTours[i][j] >= cityMapSize) {
+                        possibleTours[i][j] = cityMapSize - 1;
+                    }
+                }
+
+                // Calculate Distance
+                int mutantDistance = CalcTourDistance(possibleTours[i], cityMap, cityMapSize);
+
+                // Update the distance if the altered is better
+                if (mutantDistance < distances[i]) {
+                    distances[i] = mutantDistance;
+                }
+            }
+        }
+        /* ----------------------------------------- */
+
+        /* --- Get the best tour & show the results --- */
+        int bestIndex = 0; // Find the best tour
+        for (int i = 1; i < POSSIBLE_TOURS_SIZE; ++i) {
+            if (distances[i] < distances[bestIndex]) {
+                bestIndex = i;
+            }
+        }
+
+        // Show it
+        System.out.print("Best tour: ");
+        for (int i = 0; i < cityMapSize; ++i) {
+            System.out.print(possibleTours[bestIndex][i] + 1 + " ");
+        }
+        System.out.println();
+        System.out.println("Distance: " + distances[bestIndex]);
+        /* --------------------------------------------- */
+
+        // End log timer
+        long end = System.currentTimeMillis();
+        double timeSpent = (end - begin) / 1000.0;
+        System.out.println("\nProgram run in " + timeSpent + " seconds.");
     }
 }
---
+++ src/utils/Helper.java
@@ -0,0 +1,96 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class Helper {
+    public static void StartRandomTour(int[] tour, int maxCitySize) {
+        Random rand = new Random();
+
+        // Fill the array with sequential values
+        for (int i = 0; i < maxCitySize; ++i) {
+            tour[i] = i;
+        }
+
+        // Shuffle the array values
+        for (int i = maxCitySize - 1; i > 0; --i) {
+            int j = rand.nextInt(i + 1);
+            int temp = tour[i];
+            tour[i] = tour[j];
+            tour[j] = temp;
+        }
+    }
+
+    public static int CalcTourDistance(int[] tour, int[][] graph, int maxCitySize) {
+        int distance = 0;
+        for (int i = 0; i < maxCitySize - 1; ++i) {
+            distance += graph[tour[i]][tour[i + 1]];
+        }
+        distance += graph[tour[0]][tour[maxCitySize - 1]]; // Return to starting city
+        return distance;
+    }
+
+    public static int GenerateNewUniqueCity(int[] currentPossibleTours, int maxCitySize, int currentCityIndex) {
+        Random rand = new Random();
+
+        int newCity;
+        boolean cityAlreadyUsed;
+
+        do {
+            // Generate a new candidate city
+            newCity = rand.nextInt(maxCitySize);
+
+            // Check if the candidate city has already been used in the current tour
+            cityAlreadyUsed = false;
+            for (int k = 0; k < currentCityIndex; ++k) {
+                if (newCity == currentPossibleTours[k]) {
+                    cityAlreadyUsed = true;
+                    break;
+                }
+            }
+        } while (cityAlreadyUsed);
+
+        return newCity;
+    }
+
+    public static int[][] LoadTestCities() {
+        return new int[][] {
+                {0, 23, 10, 4, 1},
+                {23, 0, 9, 5, 4},
+                {10, 9, 0, 8, 2},
+                {4, 5, 8, 0, 11},
+                {1, 4, 2, 11, 0}
+        };
+    }
+
+    public static Map<Integer, int[][]> ReadInputFileData(String path) {
+        Map<Integer, int[][]> returnValues = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            int cityCount = Integer.parseInt(br.readLine()); // Get city count
+
+            int[][] matrix = new int[cityCount][cityCount];
+            for (int i = 0; i < cityCount; i++) {
+                String[] values = br.readLine().split(" ");
+                for (int j = 0; j < cityCount; j++) {
+                    matrix[i][j] = Integer.parseInt(values[j]);
+                }
+            }
+
+            // This is used as a way to return multiple values
+            returnValues.put(cityCount, matrix);
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not find the specified file.");
+            System.exit(0);
+        } catch (IOException e) {
+            System.out.println("An error has occurred reading the file: " + e.getMessage());
+            System.exit(0);
+        }
+        return returnValues;
+    }
+}
